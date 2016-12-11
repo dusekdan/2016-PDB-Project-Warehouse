4ba132291ae27fda611a9fed0576b9a82f793748
DusekDan <dusekdan@gmail.com>
2016-12-11 11:39:09 +0100

    Fixed - further purification done

--- PDB/src/main/java/cz/vutbr/fit/pdb/teamincredible/pdb/model/Good.java
+++ PDB/src/main/java/cz/vutbr/fit/pdb/teamincredible/pdb/model/Good.java
@@ -8,33 +8,24 @@ import oracle.ord.im.*;
  */
 public class Good
 {
-
     // GOODS_ID
     private int id;
-
     public void setId(int newId) { id = newId; }
-
     public int getId() { return id; }
 
     // GOODS_VOLUME
     private double volume;
-
     public void setVolume(double newVolume) { volume = newVolume; }
-
     public double getVolume() { return volume; }
 
     // GOODS_NAME
     private String name;
-
     public void setName(String newName) { name = newName; }
-
     public String getName() { return name; }
 
     // GOODS_PHOTO
     private OrdImage photo;
-
     public void setPhoto(OrdImage photo) { this.photo = photo;}
-
     public OrdImage getPhoto() { return photo; }
 
     // These fields should be retrieved and work with only on Database level
@@ -46,23 +37,17 @@ public class Good
 
     // GOODS_PRICE
     private double price;
-
     public double getPrice() { return price; }
-
     public void setPrice(double price) { this.price = price; }
 
     // Auxiliary variable for inserting ORDSys image
     private String imgFilePath;
-
     public String getImgFilePath() { return imgFilePath; }
-
     public void setImgFilePath(String imgFilePath) { this.imgFilePath = imgFilePath;}
 
     // Property containing real image data that can be directly rendered to scene
     private Image realImageData;
-
     public Image getRealImageData() { return realImageData; }
-
     public void setRealImageData(Image imageFromStream) { realImageData = imageFromStream; }
 
     /**
