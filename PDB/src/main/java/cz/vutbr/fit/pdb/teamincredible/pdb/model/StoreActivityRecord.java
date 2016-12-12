de1eb1b1f367e306008cf123f01f53bebbe7901d
Haskey <Suchanek.Jan@seznam.cz>
2016-12-12 07:40:58 +0100

    Refactoring, comments

--- PDB/src/main/java/cz/vutbr/fit/pdb/teamincredible/pdb/model/StoreActivityRecord.java
+++ PDB/src/main/java/cz/vutbr/fit/pdb/teamincredible/pdb/model/StoreActivityRecord.java
@@ -16,16 +16,21 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-
-
 /**
+ * Class represents one record in temporal DB.
  *
  * @author Sucha
  */
 public class StoreActivityRecord {
-    
+
+    /**
+     * information about current day, or currently set day
+     */
     public static Calendar currentDay = Calendar.getInstance();
 
+    /**
+     * load data from database for current day
+     */
     public static void loadData() {
         data = DatabaseD.GetStoreHistory();
         currentDay = Calendar.getInstance();
@@ -33,11 +38,15 @@ public class StoreActivityRecord {
         currentDay.set(Calendar.MINUTE, 0);
         currentDay.set(Calendar.SECOND, 0);
         currentDay.set(Calendar.MILLISECOND, 0);
-     
-        
+
     }
 
-    
+    /**
+     * Set currentDay to date and load data for this day
+     *
+     * @param date
+     * @return loaded data
+     */
     public static ObservableList<StoreActivityRecord> loadData(Calendar date) {
         data = DatabaseD.GetStoreHistory(date);
         currentDay = date;
@@ -47,64 +56,90 @@ public class StoreActivityRecord {
         currentDay.set(Calendar.MILLISECOND, 0);
         return data;
     }
-    
-    
-    private final SimpleStringProperty goodID;
+    /**
+     * timestamp of record validity begin time
+     */
     public final Timestamp fromTS;
+
+    /**
+     * string property with id of goods type
+     */
+    private final SimpleStringProperty goodID;
+
     public final SimpleStringProperty goodIDProperty() {
         return goodID;
     }
-    
+
+    /**
+     * string property with id of rack
+     */
     private final SimpleStringProperty stockID;
+
     public final SimpleStringProperty stockIDProperty() {
         return stockID;
     }
-    
+
+    /**
+     * integer property represents count of goods in rack
+     */
     private final SimpleIntegerProperty numOfGood;
+
     public final SimpleIntegerProperty countProperty() {
         return numOfGood;
     }
 
+    /**
+     * string prop represents begin valid time
+     */
     private final SimpleStringProperty from;
+
     public final SimpleStringProperty fromProperty() {
         return from;
     }
-    
+
+    /**
+     * string prop represents end valid time
+     */
     private final SimpleStringProperty to;
+
     public final SimpleStringProperty toProperty() {
         return to;
     }
-    
-    
-    
-    
+
+    /**
+     * constant date format string
+     */
     private static final DateFormat format = new SimpleDateFormat("d. MMMM yyyy, H:mm");
 
+    /**
+     * Constructs instance with all data set
+     *
+     * @param goodID
+     * @param stockID
+     * @param numOfGood
+     * @param from timestamp of valid time, transformed to string
+     * @param to timestamp of valid time, transformed to string
+     */
     public StoreActivityRecord(int goodID, int stockID, int numOfGood, Timestamp from, Timestamp to) {
         this.goodID = new SimpleStringProperty(String.valueOf(goodID)); // for now
         this.stockID = new SimpleStringProperty(String.valueOf(stockID)); // for now
         this.numOfGood = new SimpleIntegerProperty(numOfGood);
-   
+
         this.fromTS = from;
-        
+
         Date obj = new Date(from.getTime());
         this.from = new SimpleStringProperty(format.format(obj));
         obj = new Date(to.getTime());
         this.to = new SimpleStringProperty(format.format(obj));
     }
-    
-    
-      public static ObservableList<StoreActivityRecord> data = FXCollections.observableArrayList();
-  
-    public static void testData() {
-        // add some dummy data
-        
-        
-    }
+
+    /**
+     * list of all records in chosen time
+     */
+    public static ObservableList<StoreActivityRecord> data = FXCollections.observableArrayList();
 
     public static ObservableList<StoreActivityRecord> getData() {
         return data;
     }
-    
-    
+
 }
--- PDB/src/main/java/cz/vutbr/fit/pdb/teamincredible/pdb/view/LoginDialog.java
+++ PDB/src/main/java/cz/vutbr/fit/pdb/teamincredible/pdb/view/LoginDialog.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.vutbr.fit.pdb.teamincredible.pdb.view;
 
 
@@ -19,51 +14,73 @@ import javafx.stage.Modality;
 import javafx.util.Pair;
 
 /**
- *
+ * Represents login dialog
  * @author Sucha
  */
 public class LoginDialog extends Dialog<Pair<String, String>> {
 
-    public LoginDialog() {
-        setTitle("Přihlašte se prosím");
-        setHeaderText("Zadejte své přístupové údaje");
+     // Text constants used across the dialog
+    private static final String LOGIN_DIALOG_TITLE = "Přihlašte se, prosím";
+    private static final String LOGIN_DIALOG_HEADERTEXT = "Zadejte své přístupové údaje";
+    private static final String LOGIN_DIALOG_CONFIRM_BUTTON = "Přihlásit";
+    private static final String LOGIN_DIALOG_USERNAME_TEXTFIELD = "Uživatelské jméno:";
+    private static final String LOGIN_DIALOG_USERNAME_PASSWORD = "Heslo:";
 
+    // Controls & nodes declarations
+    private ButtonType loginButtonType;
+    private GridPane grid;
+    private TextField username;
+    private PasswordField password;
+    private Node loginButton;
+
+    
+    public LoginDialog() {
+
+        // init dialog window
+        setTitle(LOGIN_DIALOG_TITLE);
+        setHeaderText(LOGIN_DIALOG_HEADERTEXT);
         initModality(Modality.APPLICATION_MODAL);
 
-        ButtonType loginButtonType = new ButtonType("Login", ButtonData.OK_DONE);
+        loginButtonType = new ButtonType(LOGIN_DIALOG_CONFIRM_BUTTON, ButtonData.OK_DONE);
         getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
 
-        GridPane grid = new GridPane();
+        
+        // setup grid
+        grid = new GridPane();
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(20, 150, 10, 10));
 
-        TextField username = new TextField();
+        // setup controls
+        username = new TextField();
         username.setPromptText("login");
 
-        PasswordField password = new PasswordField();
+        password = new PasswordField();
         password.setPromptText("Heslo");
 
-        grid.add(new Label("Uživatelské jméno:"), 0, 0);
+        // add controls to grid
+        grid.add(new Label(LOGIN_DIALOG_USERNAME_TEXTFIELD), 0, 0);
         grid.add(username, 1, 0);
-        grid.add(new Label("Heslo:"), 0, 1);
+        grid.add(new Label(LOGIN_DIALOG_USERNAME_PASSWORD), 0, 1);
         grid.add(password, 1, 1);
 
-        final Node loginButton = getDialogPane().lookupButton(loginButtonType);
+        loginButton = getDialogPane().lookupButton(loginButtonType);
         loginButton.setDisable(true);
 
+        // add listener for check username
         username.textProperty().addListener((observable, oldValue, newValue) -> {
             loginButton.setDisable(newValue.trim().isEmpty());
         });
 
-        getDialogPane().setContent(grid);
-
+        // add result converter
         setResultConverter(dialogButton -> {
             if (dialogButton == loginButtonType) {
                 return new Pair<>(username.getText(), password.getText());
             }
             return null;
         });
+  
+        getDialogPane().setContent(grid);
 
     }
 
